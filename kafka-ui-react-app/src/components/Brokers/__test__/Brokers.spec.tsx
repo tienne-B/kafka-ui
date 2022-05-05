3036024184c0901a73ae9f4768d6a54196c545da
ssiradeghyan <ssiradeghyan@provectus.com>
2022-05-05 08:46:18 +0400

    add tests

--- kafka-ui-react-app/src/components/Brokers/__test__/Brokers.spec.tsx
+++ kafka-ui-react-app/src/components/Brokers/__test__/Brokers.spec.tsx
@@ -77,10 +77,40 @@ describe('Brokers Component', () => {
       await waitFor(() => {
         expect(fetchStatsMock.called()).toBeTruthy();
       });
-      await waitFor(() => {
-        expect(fetchBrokersMock.called()).toBeTruthy();
-      });
+
+      const onlineWidgetDef = screen.getByText('798');
       const onlineWidget = screen.getByText('of 799');
+      // const onlineIcon = screen.getAllByText()
+      expect(onlineWidgetDef).toBeInTheDocument();
+      expect(onlineWidget).toBeInTheDocument();
+    });
+    it('shows right count when inSyncReplicasCount: undefined outOfSyncReplicasCount: 1', async () => {
+      const fetchStatsMock = fetchMock.getOnce(fetchStatsUrl, {
+        ...clusterStatsPayload,
+        inSyncReplicasCount: undefined,
+        outOfSyncReplicasCount: 1,
+      });
+      renderComponent();
+      await waitFor(() => {
+        expect(fetchStatsMock.called()).toBeTruthy();
+      });
+
+      const onlineWidget = screen.getByText('of 1');
+      expect(onlineWidget).toBeInTheDocument();
+    });
+    it('shows right count when inSyncReplicasCount: 47 outOfSyncReplicasCount: undefined', async () => {
+      const fetchStatsMock = fetchMock.getOnce(fetchStatsUrl, {
+        ...clusterStatsPayload,
+        inSyncReplicasCount: 47,
+        outOfSyncReplicasCount: undefined,
+      });
+      renderComponent();
+      await waitFor(() => {
+        expect(fetchStatsMock.called()).toBeTruthy();
+      });
+      const onlineWidgetDef = screen.getByText('47');
+      const onlineWidget = screen.getByText('of 47');
+      expect(onlineWidgetDef).toBeInTheDocument();
       expect(onlineWidget).toBeInTheDocument();
     });
   });
